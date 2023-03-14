7c2aaf414684f69b7ce38bae3d291b62bfae9f9c
Mike M <mikeboi55@Mikes-MacBook-Air.local>
2023-03-14 11:22:58 -0700

    fishing count

--- script.js
+++ script.js
@@ -8,7 +8,34 @@ const unwink = () => {
     return winkingMan.src = 'resources/images/dancing-man.svg';
 }
 
-winkingMan.onmousedown = wink;
-winkingMan.onmouseup = unwink;
+winkingMan.onmouseover = wink;
+winkingMan.onmouseout = unwink;
 
+let fishingMan = document.getElementById('fish');
+
+const catchFish = () => {
+    return fishingMan.src = 'resources/images/caught-fish.svg';
+}
+
+const release = () => {
+    return fishingMan.src = 'resources/images/fishing-man.svg';
+}
+
+fishingMan.onmousedown = catchFish;
+fishingMan.onmouseup = release;
+
+let fishCount = 0
+const fishCounter = () => {
+    fishCount++;
+    if(fishCount === 30) {
+        window.alert('woah slow down there cowboy!');
+    } else if (fishCount === 75) {
+        window.alert('okay, this is the last time i bring you to my fishing hole!');
+    } else if (fishCount === 200) {
+        window.alert('you gotta stop...');
+    }
+    return document.getElementById('fish-count').innerHTML = `fish caught: ${fishCount}`;
+}
+
+fishingMan.addEventListener('click', fishCounter);
 
